52e520692ab1321d840c29433d37d62d236d58f8
lukevonbergen <57679649+lukevonbergen@users.noreply.github.com>
2025-06-12 22:52:09 +0100

    Update layout.jsx

--- app/trims/layout.jsx
+++ app/trims/layout.jsx
@@ -1,9 +1,11 @@
 import NavBar from "@/components/navigation/Nav_2";
 import Footer from "@/components/footer/Footer_1";
+import Banner from "@/components/banner/Banner_1";
 
 export default function TrimsLayout({ children }) {
   return (
     <div className="flex flex-col min-h-screen">
+      <Banner />
       <NavBar />
       <main className="flex-1">{children}</main>
       <Footer />

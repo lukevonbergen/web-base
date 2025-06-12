0fa397a3c4b7b59d5f79b1bf7f19a5f5e078f121
lukevonbergen <57679649+lukevonbergen@users.noreply.github.com>
2025-06-12 22:40:53 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -24,9 +24,9 @@ export default function HomePage() {
         />
         <Features />
         <Gallery />
-        <CTA />
         <Testimonials />
         <Pricing />
+        <CTA />
         <Footer />
       </main>
   );

ce6adc164ad6c2fafb3907d1526b69ba6b37e53d
lukevonbergen <57679649+lukevonbergen@users.noreply.github.com>
2025-06-12 13:07:51 +0000

    mjs removal

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: 'var(--font-geist-sans)',
-        mono: 'var(--font-geist-mono)',
-      },
-    },
+    extend: {},
   },
   plugins: [],
-}
+};

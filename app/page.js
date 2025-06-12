4d6b7f0cd8e327808af73f62d0ba82ef14efc93c
lukevonbergen <57679649+lukevonbergen@users.noreply.github.com>
2025-06-12 22:19:02 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import HeroSection from "@/components/HeroSection";
 import Features from "@/components/Features"
 import Pricing from "@/components/Pricing"
 import Footer from "@/components/Footer"
-import NavBar from "@/components/NavBar"
+import NavBar from "@/components/navigation/Nav_1"
 import Gallery from "@/components/Gallery"
 
 export default function HomePage() {

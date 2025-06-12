18f6b730e7c421f04beab6a7b19536a35159baef
lukevonbergen <57679649+lukevonbergen@users.noreply.github.com>
2025-06-12 22:12:00 +0100

    Update page.jsx

--- app/trims/page.jsx
+++ app/trims/page.jsx
@@ -1,1 +1,13 @@
+import Features from "@/components/Features"
+import Pricing from "@/components/Pricing"
+import Gallery from "@/components/Gallery"
 
+export default function HomePage() {
+  return (
+      <main>
+        <Features />
+        <Gallery />
+        <Pricing />
+      </main>
+  );
+}
